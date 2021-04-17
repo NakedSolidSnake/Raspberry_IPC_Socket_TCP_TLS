2231199c1550155d41ee3db3274c84b7cf4bbed3
Cristiano Silva <cristianosstec@gmail.com>
2021-04-17 17:18:43 -0300

    refactor

--- src/button_interface.c
+++ src/button_interface.c
@@ -8,6 +8,22 @@
 
 #define _1ms    1000
 
+static void wait_press(Button_Data *button)
+{
+    while (true)
+    {
+        if (!button->interface->Read(button->object))
+        {
+            usleep(_1ms * 100);
+            break;
+        }
+        else
+        {
+            usleep(_1ms);
+        }
+    }
+}
+
 bool Button_Run(TCP_Client_t *client, Button_Data *button)
 {
     static int state = 0;
@@ -19,20 +35,8 @@ bool Button_Run(TCP_Client_t *client, Button_Data *button)
 
     while(true)
     {
-        while (true)
-        {
-            if (!button->interface->Read(button->object))
-            {
-                usleep(_1ms * 100);
-                state ^= 0x01;
-                break;
-            }
-            else
-            {
-                usleep(_1ms);
-            }
-        }
-
+        wait_press(button);
+        state ^= 0x01;
         TCP_Client_Connect(client, &state);
     }
 }
